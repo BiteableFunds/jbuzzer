67e7b619affa8ef76af54b0aa04e97ab21a8eb08
BiteableFunds <131499753+BiteableFunds@users.noreply.github.com>
2023-11-23 08:39:09 -0500

    Update bzrscript.js

--- bzrscript.js
+++ bzrscript.js
@@ -1,9 +1,8 @@
-buzzer = document.getElementById("bzr");
-buzzerdisplay = document.getElementById("bzrdisplay");
-displayText = buzzerdisplay.innerHTML;
+const buzzer = document.getElementById("bzr");
+const buzzerdisplay = document.getElementById("bzrdisplay");
 
 function buzzerPress() {
 	let unixtimestamp = Date.now();
 	var date = new Date(unixtimestamp).toLocaleTimeString("en-US");
-	displayText = `buzzed in at: ${date} ${unixtimestamp}`;
+	buzzerdisplay.innerHTML = `buzzed in at: ${date} ${unixtimestamp}`;
 }
